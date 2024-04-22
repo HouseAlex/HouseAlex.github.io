632bd2ec9680e47d64cda2df7b6121e78faa52d3
tedcordonnier <tedcordonnier@gmail.com>
2024-04-22 15:52:14 -0400

    Fix barchart2

--- js/main.js
+++ js/main.js
@@ -39,6 +39,8 @@ d3.csv('data/Supernatural.csv')
                 characterNetwork.UpdateVis();
                 barchart.data = mainCastDialogueOG
                 barchart.UpdateVis();
+                barchart2.data = mainCastDialogueOG
+                barchart2.UpdateVis();
             }
             else {
                 let filtered = mainCastDialogue.filter(d => d.season == this.value)
@@ -46,6 +48,8 @@ d3.csv('data/Supernatural.csv')
                 characterNetwork.UpdateVis();
                 barchart.data = filtered
                 barchart.UpdateVis();
+                barchart2.data = filtered
+                barchart2.UpdateVis();
             }
         })
 
